bf72e4e70ce4dda244b2787d857174d78891f4a2
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-13 14:53:41 +0100

    ändrinfg

--- src/Stjarntecken.java
+++ src/Stjarntecken.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Stjarntecken {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int dayBorn;
